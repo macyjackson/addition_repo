a25ba2407a8497bbb85662c9e44d640731f3d18f
Macy <macyjackson@ymail.com>
2017-07-12 11:03:50 -0400

    addition initial file

--- addition.rb
+++ addition.rb
@@ -1,6 +1,9 @@
-x = 1 + 1
-y = 2 + 2
-z = 3 + 3
-print "1 + 1 = #{(x)}"
-print "2 + 2 = #{(y)}"
-print "3 + 3 = #{(z)}"
+a = 2
+b = 3
+c = 1
+x = a + b
+y = a + c
+z = c + a
+print "a + b = #{(x)}"
+print "a + c = #{(y)}"
+print "c + a = #{(z)}"
